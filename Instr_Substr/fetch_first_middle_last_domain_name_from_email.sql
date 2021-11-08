8db16adb310d39fc201e00f76e1f6ba27d0680f1
Gajanana Ganjigatti <40160330+Gaju27@users.noreply.github.com>
2021-11-08 17:08:09 +0530

    Add files via upload

---
+++ Instr_Substr/fetch_first_middle_last_domain_name_from_email.sql
@@ -0,0 +1,344 @@
+
+SET DEFINE OFF;
+ALTER SESSION SET CURRENT_SCHEMA = YOUR_SCHEMA;
+
+DROP TABLE email_t;
+
+CREATE TABLE email_t
+(
+    EMAIL    VARCHAR2 (200 CHAR) NOT NULL
+);
+
+BEGIN
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Bruce.BERNST.Ernst@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Jose Manuel.JMURMAN.Urman@msn.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Alexander.AKHOO.Khoo@yahoo.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Renske.RLADWIG.Ladwig@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Oliver.OTUVAULT.Tuvault@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Patrick.PSULLY.Sully@msn.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Sarath.SSEWALL.Sewall@yahoo.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Alexis.ABULL.Bull@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Vance.VJONES.Jones@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Mozhe.MATKINSO.Atkinson@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('John.JRUSSEL.Russell@yahoo.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Charles.CJOHNSON.Johnson@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Michael.MROGERS.Rogers@msn.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Stephen.SSTILES.Stiles@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Mattea.MMARVINS.Marvins@yahoo.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Hermann.HBAER.Baer@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Lex.LDEHAAN.De Haan@msn.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Diana.DLORENTZ.Lorentz@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Ki.KGEE.Gee@yahoo.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Hazel.HPHILTAN.Philtanker@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('John.JSEO.Seo@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('David.DBERNSTE.Bernstein@msn.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Peter.PHALL.Hall@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Jean.JFLEAUR.Fleaur@yahoo.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Sarah.SBELL.Bell@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Samuel.SMCCAIN.McCain@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Jonathon.JTAYLOR.Taylor@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Curtis.CDAVIES.Davies@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Alexander.AHUNOLD.Hunold@yahoo.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Matthew.MWEISS.Weiss@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Julia.JNAYER.Nayer@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Steven.SMARKLE.Markle@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('TJ.TJOLSON.Olson@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Nanette.NCAMBRAU.Cambrault@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Kelly.KCHUNG.Chung@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Pat.PFAY.Fay@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Susan.SMAVRIS.Mavris@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('David.DAUSTIN.Austin@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Nancy.NGREENBE.Greenberg@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Adam.AFRIPP.Fripp@msn.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Shanta.SVOLLMAN.Vollman@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Kevin.KMOURGOS.Mourgos@yahoo.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('JASon.JMALLIN.Mallin@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Peter.PVARGAS.VargAS@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Karen.KPARTNER.Partners@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Alberto.AERRAZUR.Errazuriz@msn.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Danielle.DGREENE.Greene@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Kimberely.KGRANT.Grant@yahoo.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Martha.MSULLIVA.Sullivan@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Christopher.COLSEN.Olsen@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('DouglAS.DGRANT.Grant@msn.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Shelley.SHIGGINS.Higgins@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Daniel.DFAVIET.Faviet@yahoo.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('John.JCHEN.Chen@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Den.DRAPHEAL.Raphaely@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Sigal.STOBIAS.TobiAS@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Guy.GHIMURO.Himuro@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Lindsey.LSMITH.Smith@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('David.DLEE.Lee@yahoo.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Harrison.HBLOOM.Bloom@msn.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Tayler.TFOX.Fox@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('William.WSMITH.Smith@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Jack.JLIVINGS.Livingston@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Anthony.ACABRIO.Cabrio@yahoo.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Timothy.TGATES.Gates@msn.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Alana.AWALSH.Walsh@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Kevin.KFEENEY.Feeney@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Irene.IMIKKILI.Mikkilineni@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Julia.JDELLING.Dellinger@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Jennifer.JWHALEN.Whalen@yahoo.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Steven.SKING.King@msn.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Ismael.ISCIARRA.Sciarra@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Luis.LPOPP.Popp@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Karen.KCOLMENA.Colmenares@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Amit.ABANDA.Banda@yahoo.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Jennifer.JDILLY.Dilly@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Britney.BEVERETT.Everett@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Janette.JKING.King@msn.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Elizabeth.EBATES.Bates@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Eleni.EZLOTKEY.Zlotkey@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Donald.DOCONNEL.OConnell@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Neena.NKOCHHAR.Kochhar@yahoo.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Valli.VPATABAL.Pataballa@msn.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Payam.PKAUFLIN.Kaufling@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('James.JAMRLOW.Marlow@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Joshua.JPATEL.Patel@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Trenna.TRAJS.Rajs@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Randall.RMATOS.Matos@yahoo.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Gerald.GCAMBRAU.Cambrault@msn.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Peter.PTUCKER.Tucker@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Allan.AMCEWEN.McEwen@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Clara.CVISHNEY.Vishney@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Alyssa.AHUTTON.Hutton@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Nandita.NSARCHAN.Sarchand@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Louise.LDORAN.Doran@yahoo.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Girard.GGEONI.Geoni@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Michael.MHARTSTE.Hartstein@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('William.WGIETZ.Gietz@msn.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Shelli.SBAIDA.Baida@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('James.JLANDRY.Landry@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Laura.LBISSOT.Bissot@yahoo.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Sundar.SANDE.Ande@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Sundita.SKUMAR.Kumar@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Ellen.EABEL.Abel@gmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Winston.WTAYLOR.Taylor@msn.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Randall.RPERKINS.Perkins@hotmail.com');
+
+INSERT INTO email_t (EMAIL)
+     VALUES ('Lisa.LOZER.Ozer@yahoo.com');
+END;
+/
+
+COMMIT;
+
+
+SELECT email,
+SUBSTR(email,1,INSTR(email,'.',1,1)-1) AS fist_name,  
+SUBSTR(email,INSTR(email,'.',1,1)+1,INSTR(email,'.',1,2)-INSTR(email,'.',1,1)-1) AS middle_name ,
+SUBSTR(email,INSTR(email,'@',1,1)+1, INSTR(email,'.',1,3)-INSTR(email,'@',1,1)-1) AS domain_name 
+FROM email_t;
